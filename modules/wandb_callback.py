c9b490b131f1afb3f4788cf2c3da7df1d74dd7cb
Gregor Krzmanc <gregor.krzmanc@cern.ch>
2023-07-10 19:06:26 +0200

    fix epoch number wandb

--- modules/wandb_callback.py
+++ modules/wandb_callback.py
@@ -12,7 +12,7 @@ import wandb
 class wandbCallback(Callback):
     # Just log everything to wandb
     def __init__(self):
-        self.curr_step=0
+        self.curr_epoch=0
 
     def _record_data(self,logs,record_epoch=None):
         #step_number = self.params['steps'] * self.epoch + self.params['batch']
@@ -20,16 +20,13 @@ class wandbCallback(Callback):
         #print("step_number", step_number, "logs", logs)
         # Also log separate metrics on ends of epochs
         if record_epoch is not None:
-            wandb.log({"epoch_" + k: v for k, v in logs.items()})
+            wandb.log({"epoch_" + k: v for k, v in logs.items()}, step=self.curr_epoch)
 
     def on_batch_end(self,batch,logs={}):
         self._record_data(logs)
-        self.curr_step += 1
 
     def on_epoch_end(self,epoch,logs={}):
         self._record_data(logs)
-        self.curr_step += 1
+        self.curr_epoch += 1
         self._record_data(logs,record_epoch=True)
-
-
 
