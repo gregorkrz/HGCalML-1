c3d3a67fd25cd7ff0609b53a6dd4be6f2120f2a4
Gregor Krzmanc <gregor.krzmanc@cern.ch>
2023-07-10 15:55:50 +0200

    add separate epoch logs for wandb

--- modules/wandb_callback.py
+++ modules/wandb_callback.py
@@ -14,10 +14,13 @@ class wandbCallback(Callback):
     def __init__(self):
         self.curr_step=0
 
-    def _record_data(self,logs):
+    def _record_data(self,logs,record_epoch=None):
         #step_number = self.params['steps'] * self.epoch + self.params['batch']
         wandb.log(logs)
         #print("step_number", step_number, "logs", logs)
+        # Also log separate metrics on ends of epochs
+        if record_epoch is not None:
+            wandb.log({"epoch_" + k: v for k, v in logs.items()})
 
     def on_batch_end(self,batch,logs={}):
         self._record_data(logs)
@@ -26,4 +29,7 @@ class wandbCallback(Callback):
     def on_epoch_end(self,epoch,logs={}):
         self._record_data(logs)
         self.curr_step += 1
+        self._record_data(logs,record_epoch=True)
+
+
 
